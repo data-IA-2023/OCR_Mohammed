646eb3fcf9f32e6adbe0a28be3304780d0ddba54
COMEWLECOME <MOHASOFT@GMAIL.COM>
2024-03-31 15:06:16 +0200

    la premier app qui affiche les factures

--- App_display_invoice.py
+++ App_display_invoice.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from ORM import Client, Facture, DetailFacture, connectBd
+import os
 
 # Fonction pour se connecter à la base de données
 def get_session():
@@ -18,38 +19,72 @@ def get_facture_ids(session):
     facture_ids = [facture.QRid for facture in session.query(Facture.QRid).all()]
     return facture_ids
 
+# Fonction pour trouver le nom du fichier image correspondant au QRid
+def find_facture_image(qr_id):
+    facture_images = [f for f in os.listdir('factures') if f.startswith(qr_id)]
+    if facture_images:
+        return facture_images[0]
+    else:
+        return None
+
 # Fonction pour afficher les informations de la facture
 def display_invoice_info(session, qr_id):
     facture = session.query(Facture).filter_by(QRid=qr_id).first()
     if facture:
-        st.subheader("Facture")
-        st.write(f"INVOICE: {facture.QRid}")
-        st.write(f"Issue date: {facture.QRdate.strftime('%Y-%m-%d')}")
-        st.write(f"Bill to: {facture.client.client}")
-        st.write(f"Adress: {facture.client.adresse}")
+        col1, col2 = st.columns([1, 1] ) #, gap="small"
 
-        # st.subheader("Détails de la Facture")
-        details = session.query(DetailFacture).filter_by(facture_id=qr_id).all()
-        df = pd.DataFrame([(detail.label, detail.quantite, detail.prix) for detail in details], 
-                          columns=['Label', 'Quantité', 'Prix'])
-        st.dataframe(df)
+        with col1:
+            st.subheader("Facture")
+            st.write(f"INVOICE: {facture.QRid}")
+            st.write(f"Issue date: {facture.QRdate.strftime('%Y-%m-%d')}")
+            st.write(f"Bill to: {facture.client.client}")
+            st.write(f"Adress: {facture.client.adresse}")
 
-        st.subheader("TOTAL")
-        st.write(f"Total : {facture.total_value}")
-        st.write(f"Total Calculated: {facture.total_Calculated}")
-        st.write(f"Delta: {facture.delta}")
+            # st.subheader("Détails de la Facture")
+            details = session.query(DetailFacture).filter_by(facture_id=qr_id).all()
+            df = pd.DataFrame([(detail.label, detail.quantite, detail.prix) for detail in details], 
+                            columns=['Label', 'Quantité', 'Prix'])
+            st.dataframe(df)
+
+            st.subheader("TOTAL")
+            st.write(f"Total : {facture.total_value}")
+            st.write(f"Total Calculated: {facture.total_Calculated}")
+            st.write(f"Delta: {facture.delta}")
+
+        with col2:
+            st.subheader("Image Source")
+            facture_image = find_facture_image(qr_id)
+            if facture_image:
+                st.image(f"factures/{facture_image}", caption='Facture') #, width=1000
+            else:
+                st.write("Aucune image de facture trouvée.")
+
     else:
         st.write("Aucune facture trouvée avec ce QRid.")
 
 # Fonction principale de l'application
 def main():
-    # st.title("Affichage des Factures et Détails")
+    # Supprimer les marges à gauche et à droite
+    # st.markdown(
+    #     """
+    #     <style>
+    #     .reportview-container .main .block-container {
+    #         padding-top: 0rem !important;
+    #         padding-right: 0rem !important;
+    #         padding-left: 0rem !important;
+    #         padding-bottom: 0rem !important;
+    #     }
+    #     </style>
+    #     """,
+    #     unsafe_allow_html=True
+    # )
+    st.set_page_config(layout="wide")
     session = get_session()
 
     if session:
         # Récupérer les IDs de facture depuis la base de données
         facture_ids = get_facture_ids(session)
-
+        st.sidebar.image('logo.jpg')
         # Demander à l'utilisateur d'entrer le QRid de la facture avec auto-complétion
         qr_id = st.sidebar.selectbox("Entrez l'ID de la facture :", options=facture_ids)
 
